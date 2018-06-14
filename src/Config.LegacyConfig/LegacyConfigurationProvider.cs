b483e895d0beaaf5e685e45c54038ed4981cf9e4
Haseeb Ahmed <v-hahmed@outlook.com>
2018-06-14 19:01:02 +0500

    A very basic implementation of the LegacyConfigurationProvider is done, will add support for choosing the config in a later commit.

---
+++ src/Config.LegacyConfig/LegacyConfigurationProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace HA.Extensions.Configuration.Legacy
+{
+    using System.Configuration;
+
+    public class LegacyConfigurationProvider : ConfigurationProvider, IConfigurationSource
+    {
+        public override void Load()
+        {
+            foreach (ConnectionStringSettings connectionString in ConfigurationManager.ConnectionStrings)
+            {
+                Data.Add($"ConnectionStrings:{connectionString.Name}", connectionString.ConnectionString);
+            }
+
+            foreach (var settingKey in ConfigurationManager.AppSettings.AllKeys)
+            {
+                Data.Add(settingKey, ConfigurationManager.AppSettings[settingKey]);
+            }
+        }
+
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return this;
+        }
+    }
+}
