a51304a312ba6032fc91bd318dde30bb036dcffb
Haseeb Ahmed <v-hahmed@outlook.com>
2018-06-15 02:22:32 +0500

    Final set of improvements for Travis CI config and build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 dotnet restore
 dotnet pack src/Config.LegacyConfig/Config.LegacyConfig.csproj
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [	"${TRAVIS_BRANCH}" == "${MY_DEFAULT_BRANCH}" ]; then
+echo "Is Pull Request?" $TRAVIS_PULL_REQUEST
+echo "Publish Branch: " $MY_PUBLISH_BRANCH
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [	"${TRAVIS_BRANCH}" == "${MY_PUBLISH_BRANCH}" ]; then
   dotnet nuget push src/Config.LegacyConfig/bin/**/*.nupkg -s https://api.nuget.org/v3/index.json -k keygoeshere
 fi
