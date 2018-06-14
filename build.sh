73c3bd437fb61fe0aa57a545fc7c385a1c519028
Haseeb Ahmed <v-hahmed@outlook.com>
2018-06-15 01:40:25 +0500

    Some improvements to the build.sh file.

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 dotnet restore
 dotnet pack src/Config.LegacyConfig/Config.LegacyConfig.csproj
-dotnet nuget push src/Config.LegacyConfig/bin/**/*.nupkg -s https://api.nuget.org/v3/index.json -k keygoeshere
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == "${MY_DEFAULT_BRANCH}" ]; then
+  dotnet nuget push src/Config.LegacyConfig/bin/**/*.nupkg -s https://api.nuget.org/v3/index.json -k keygoeshere
+fi
